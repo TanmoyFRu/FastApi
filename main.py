5a3c365e89673db878de7b611e08e6e84b1ed93b
TanmoyFRu <tanmoydn2003@gmail.com>
2024-11-08 20:04:59 +0530

    updateo

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI
 
+# creating an Instance --> app
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data':{
-        "name":"Tanmoy Debnath"
-    }}
+@app.get('/blog')
+# only GET 10 published blogs
+# path operation decorator --> @app
+# operation --> GET
+# path --> ("/")
+def index(limit,published : bool):
+# path operation function --> def index()
+ if published:
+    return {'data': f'{limit} published blog from the db'}
+ else:
+    return {'data': f'{limit} blog from the db'}
 
-@app.get('/about')
-def about():
-    return {'data':{
-        "version":"v1.0.0",
-        "author":"Tanmoy Debnath",
-        "description":"This is a simple API developed using FastAPI framework."
-    }}
+    
+@app.get('/blog/unpublished')
+def unpublished(): 
+    return {'data': "all Unpublished Blogs"}
+    
+@app.get('/blog/{id}')
+# path operation decorator --> @app
+# operation --> GET
+# path --> ("/about")
+# {id} --> path parameter
+def show(id : int):
+    #path operation function --> def show()
+    #fetch blog id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id): 
+    return {'data':{'comments':{'1','2'}}}
+    
