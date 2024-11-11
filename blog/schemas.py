48b913ba7d98bf0396b7fc655caeca56f083e46b
TanmoyFRu <tanmoydn2003@gmail.com>
2024-11-11 21:13:04 +0530

    test

--- blog/schemas.py
+++ blog/schemas.py
@@ -5,3 +5,8 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title:str
     body:str
+    
+    
+class Show_Blog(Blog):
+    class Config():
+        orm_mode = True
--- main.py
+++ main.py
@@ -20,10 +20,14 @@ def get_db():
 def Landing_page_blog():
     return "Welcome to my Blog"
 
+
+
 @app.get("/blog")
 def get_all_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
 
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
@@ -34,24 +38,25 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog_by_id(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    
     blog.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
     
-@app.get('/blog/{id}', status_code=200)
-def get_blog_by_id(id: int, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.Show_Blog)
+def get_blog_by_id(id: int, response : Response,db: Session = Depends(get_db)):
     blog_to_get = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog_to_get:
        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Blog with the id {id} is not available")
     return blog_to_get
+
 
 
 @app.put('/blog/{id}', status_code = status.HTTP_202_ACCEPTED)
