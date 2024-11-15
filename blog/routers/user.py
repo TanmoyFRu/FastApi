49e20f0fb045d96603182f3f94154cf9d06db802
TanmoyFRu <tanmoydn2003@gmail.com>
2024-11-16 01:14:25 +0530

    "Removed current_user dependency from create_user endpoint, added current_user dependency to get_user_by_id endpoint"

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -12,10 +12,10 @@ get_db = database.get_db
 
 
 @router.post('', response_model = schemas.Show_User)
-def create_user(request : schemas.User, db: Session = Depends(get_db), current_user : schemas.User = Depends(oauth2.get_current_user)):
+def create_user(request : schemas.User, db: Session = Depends(get_db)):
     return userRepository.create(request,db)
 
 
 @router.get('/{id}', response_model = schemas.Show_User, status_code=status.HTTP_200_OK)
-def get_user_by_id(id: int, db: Session = Depends(get_db)):
+def get_user_by_id(id: int, db: Session = Depends(get_db), current_user : schemas.User = Depends(oauth2.get_current_user)):
     return userRepository.get_all(id,db)
